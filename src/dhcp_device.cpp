085a0870e0a9b4829ac5e4c0a53e7fab175015a5
jcaiMR <jcai@microsoft.com>
2023-09-11 09:17:03 +0000

    refine counting logic

--- src/dhcp_device.cpp
+++ src/dhcp_device.cpp
@@ -357,7 +357,10 @@ static void handle_dhcp_option_53(std::string &sock_if,
     std::string context_if(context->intf);
 
     // count for incomming physical interfaces
-    increase_db_counter(sock_if, dhcp_option[2], dir);
+    if (context_if.compare(sock_if) != 0) {
+        increase_db_counter(sock_if, dhcp_option[2], dir);
+        return;
+    }
 
     switch (dhcp_option[2])
     {
@@ -374,9 +377,7 @@ static void handle_dhcp_option_53(std::string &sock_if,
             context->counters[DHCP_COUNTERS_CURRENT][dir][dhcp_option[2]]++;
             aggregate_dev.counters[DHCP_COUNTERS_CURRENT][dir][dhcp_option[2]]++;
             // count for device context interfaces (-d -u -m)
-            if (context_if.compare(sock_if) != 0) {
-                increase_db_counter(context_if, dhcp_option[2], dir);
-            }
+            increase_db_counter(context_if, dhcp_option[2], dir);
         }
         break;
     // DHCP messages send by server
@@ -387,16 +388,14 @@ static void handle_dhcp_option_53(std::string &sock_if,
             (!context->is_uplink && dir == DHCP_TX)) {
             context->counters[DHCP_COUNTERS_CURRENT][dir][dhcp_option[2]]++;
             aggregate_dev.counters[DHCP_COUNTERS_CURRENT][dir][dhcp_option[2]]++;
-            if (context_if.compare(sock_if) != 0) {
-                increase_db_counter(context_if, dhcp_option[2], dir);
-            }
+            // count for device context interfaces (-d -u -m)
+            increase_db_counter(context_if, dhcp_option[2], dir);
         }
         break;
     default:
         syslog(LOG_WARNING, "handle_dhcp_option_53(%s): Unknown DHCP option 53 type %d", context->intf, dhcp_option[2]);
-        if (context_if.compare(sock_if) != 0) {
-            increase_db_counter(context_if, dhcp_option[2], dir);
-        }
+        // count for device context interfaces (-d -u -m)
+        increase_db_counter(context_if, dhcp_option[2], dir);
         break;
     }
 }
@@ -507,9 +506,14 @@ static void read_tx_callback(int fd, short event, void *arg)
             continue;
         }
         std::string intf(interfaceName);
-        context = interface_to_dev_context(devices, intf);
+        context = find_device_context(devices, intf);
         if (context) {
             client_packet_handler(intf, context, tx_recv_buffer, buffer_sz, DHCP_TX);
+        } else {
+            context = interface_to_dev_context(devices, intf);
+            if (context) {
+                client_packet_handler(intf, context, tx_recv_buffer, buffer_sz, DHCP_TX);
+            }
         }
     }
 }
@@ -541,10 +545,15 @@ static void read_rx_callback(int fd, short event, void *arg)
             continue;
         }
         std::string intf(interfaceName);
-        context = interface_to_dev_context(devices, intf);
+        context = find_device_context(devices, intf);
         if (context) {
             client_packet_handler(intf, context, rx_recv_buffer, buffer_sz, DHCP_RX);
-        }
+        } else {
+            context = interface_to_dev_context(devices, intf);
+            if (context) {
+                client_packet_handler(intf, context, rx_recv_buffer, buffer_sz, DHCP_RX);
+            }
+        } 
     }
 }
 
